7513bb9992e79e9c0cbe24c81a33494996a1a947
TonsnakeLin <lpbgytong@163.com>
2022-10-21 22:17:56 +0800

    test wakeup time
    Signed-off-by: TonsnakeLin <lpbgytong@163.com>

--- README.md
+++ README.md
@@ -1,1 +1,94 @@
-hello
+# Thread waked Up Time
+
+## X86 Enviroment
+
+### CPU
+
+Architecture:          x86_64
+CPU op-mode(s):        32-bit, 64-bit
+Byte Order:            Little Endian
+CPU(s):                8
+On-line CPU(s) list:   0-7
+Thread(s) per core:    1
+Core(s) per socket:    2
+
+座：                 4
+NUMA 节点：         1
+厂商 ID：           GenuineIntel
+CPU 系列：          15
+型号：              6
+型号名称：        Common KVM processor
+步进：              1
+CPU MHz：             2199.998
+BogoMIPS：            4399.99
+超管理器厂商：  KVM
+虚拟化类型：     完全
+L1d 缓存：          32K
+L1i 缓存：          32K
+L2 缓存：           4096K
+L3 缓存：           16384K
+NUMA 节点0 CPU：    0-7
+Flags:                 fpu vme de pse tsc msr pae mce cx8 apic sep mtrr pge mca cmov pat pse36 clflush mmx fxsr sse sse2 ht syscall nx lm constant_tsc nopl xtopology eagerfpu pni cx16 x2apic hypervisor lahf_lm
+
+### OS
+
+Linux CentOS76_VM 3.10.0-1127.el7.x86_64 #1 SMP Tue Mar 31 23:36:51 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux
+
+
+
+### DATA
+
+we waked up thread 100 times every round.
+
+| round | avg   | extra info                               |
+| ----- | ----- | ---------------------------------------- |
+| 1     | 58.51 | p80: Some(60), p95: Some(91), max: Some(1105) |
+| 2     | 43.48 | p80: Some(57), p95: Some(80), max: Some(352) |
+| 3     | 46.31 | p80: Some(60), p95: Some(101), max: Some(303) |
+| 4     | 44.54 | p80: Some(57), p95: Some(99), max: Some(276) |
+| 5     | 45.4  | p80: Some(66), p95: Some(92), max: Some(208) |
+|       |       |                                          |
+
+
+
+## ARM Enviroment
+
+### CPU
+
+[tidb5@localhost CppProgramming]$ lscpu
+Architecture:          aarch64
+Byte Order:            Little Endian
+CPU(s):                64
+On-line CPU(s) list:   0-63
+Thread(s) per core:    1
+Core(s) per socket:    32
+Socket(s):             2
+NUMA node(s):          2
+Model:                 0
+BogoMIPS:              200.00
+L1d cache:             64K
+L1i cache:             64K
+L2 cache:              512K
+L3 cache:              32768K
+NUMA node0 CPU(s):     0-31
+NUMA node1 CPU(s):     32-63
+Flags:                 fp asimd evtstrm aes pmull sha1 sha2 crc32 atomics fphp asimdhp cpuid asimdrdm jscvt fcma dcpop asimddp asimdfhm
+
+### OS
+
+Linux localhost.localdomain 4.18.0-305.10.2.el7.aarch64 #1 SMP Fri Jul 23 21:19:40 UTC 2021 aarch64 aarch64 aarch64 GNU/Linux
+
+### DATA
+
+we waked up thread 100 times every round.
+
+| round | avg  | extra info |
+| ----- | ---- | ---------- |
+|       |      |            |
+|       |      |            |
+|       |      |            |
+|       |      |            |
+|       |      |            |
+
+
+
--- RustProgramming/src/main.rs
+++ RustProgramming/src/main.rs
@@ -1,3 +1,175 @@
+#![allow(dead_code, unused_variables, unused_imports, non_snake_case)]
+use core::time;
+use std::sync::{Arc, Mutex, Condvar};
+use std::sync::mpsc::channel;
+use std::time::Instant;
+use std::thread;
+
+
+fn wait_thread_started() {
+    let pair = Arc::new((Mutex::new(false), Condvar::new()));
+    let pair2 = Arc::clone(&pair);
+
+    // Inside of our lock, spawn a new thread, and then wait for it to start.
+    thread::spawn(move|| {
+        let (lock, cvar) = &*pair2;
+        println!("notifier: enter lock");
+        let mut started = lock.lock().unwrap();
+        *started = true;
+        // We notify the condvar that the value has changed.
+        println!("notifier: notify condvar");
+        cvar.notify_one();
+    });
+
+    // Wait for the thread to start up.
+    let (lock, cvar) = &*pair;
+    println!("waiter: enter lock");
+    let mut started = lock.lock().unwrap();
+    while !*started {
+        println!("waiter: enter wait");
+        started = cvar.wait(started).unwrap();
+    }
+}
+
+fn thread_wake_up_time() {
+    let (sender, receiver) = channel();
+    let pair: Arc<(Mutex<bool>, Condvar)> = Arc::new((Mutex::new(false), Condvar::new()));
+    let pair2:  Arc<(Mutex<bool>, Condvar)> = Arc::clone(&pair);
+    let mut vec2 = Vec::new();
+    let mut vec1 = Vec::new();
+    let one_second = time::Duration::from_secs(1);
+
+    thread::spawn(move|| {
+        let (lock, cvar) = &*pair2;        
+        for _i in 0..100 {
+            println!("waiter: lock");
+            let mut flag = lock.lock().unwrap();
+            println!("enter condvar wait");
+            *flag = true;
+            flag = cvar.wait(flag).unwrap();
+            let now = Instant::now();
+            drop(flag);
+            
+            println!("after time:{:?}", now);
+            sender.send(now).unwrap();
+            println!("waiter: unlock");
+        }
+    });    
+    thread::sleep(one_second);
+    let (lock, cvar) = &*pair;
+    for _i in 0..100 {
+        println!("notifier: lock");
+        let flag = lock.lock().unwrap();
+        // We notify the condvar that the value has changed.
+        let now = Instant::now();        
+        cvar.notify_one();
+        drop(flag);
+        println!("before time:{:?}", now);
+        vec1.push(now);  
+        let now2 = receiver.recv().unwrap();
+        vec2.push(now2);
+        println!("notifier: unlock");
+        thread::sleep(one_second);
+    }
+
+    let len = std::cmp::min(vec1.len(), vec2.len());
+    let mut vec3 = Vec::new();
+    let mut sum = 0;
+    for _i in 0..len {
+        let t1 = vec1.get(_i).unwrap();
+        let t2 = vec2.get(_i).unwrap();
+        let delta = t2.duration_since(t1.clone()).as_micros();
+        sum += delta;
+        vec3.push(delta);
+    }
+    // println!("vec1: {:?}", vec1);
+    // println!("vec2: {:?}", vec2);
+    
+    println!("avg: {:?}", sum as f64/len as f64);
+    vec3.sort();
+    println!("vec3: {:?}", vec3);
+
+
+    let p80 = (len as f64 * 0.8) as usize;
+    let p95 = (len as f64 * 0.95) as usize;
+    
+    println!("p80: {:?}, p95: {:?}, max: {:?}", vec3.get(p80), vec3.get(p95), vec3.get(len - 1));
+}
+
+
+fn thread_wake_up_time2() {
+    let pair = Arc::new((Mutex::new(false), Condvar::new()));
+    let pair2 = Arc::clone(&pair);
+
+    // Inside of our lock, spawn a new thread, and then wait for it to start.
+    thread::spawn(move|| {
+        let (lock, cvar) = &*pair2;
+        for _i in 0..2 {
+            thread::sleep(time::Duration::from_secs(1));            
+            println!("notifier: enter lock");
+            let mut started = lock.lock().unwrap();
+            *started = true;
+            // We notify the condvar that the value has changed.
+            println!("notifier: notify condvar");
+            cvar.notify_one();
+            let now = Instant::now();
+            println!("before time:{:?}", now);
+        }        
+    });
+
+    // Wait for the thread to start up.
+    let (lock, cvar) = &*pair;
+    println!("waiter: enter lock");
+    for _i in 0..2 {
+        let mut started = lock.lock().unwrap();    
+        println!("waiter: enter wait");
+        started = cvar.wait(started).unwrap();
+        let now = Instant::now();
+        println!("after time:{:?}", now);
+        drop(started);
+    }  
+}
+
+fn thread_wake_up_time3() {
+    let pair = Arc::new((Mutex::new(false), Condvar::new()));
+    let pair2 = Arc::clone(&pair);
+
+    // Inside of our lock, spawn a new thread, and then wait for it to start.
+    thread::spawn(move|| {
+        let (lock, cvar) = &*pair2;
+        for _i in 0..2 {
+                       
+            println!("waiter: enter lock");
+            let mut started = lock.lock().unwrap();
+            *started = true;
+            // We notify the condvar that the value has changed.
+            println!("waiter: enter wait");
+            started = cvar.wait(started).unwrap();
+            drop(started);
+            let now = Instant::now();
+            println!("after time:{:?}", now);
+        }        
+    });
+
+    thread::sleep(time::Duration::from_secs(1)); 
+    // Wait for the thread to start up.
+    let (lock, cvar) = &*pair;
+    println!("notify: enter lock");
+    for _i in 0..2 {
+        let started = lock.lock().unwrap();    
+        println!("notify: enter wait");
+        let now = Instant::now();
+        println!("before time:{:?}", now);
+        cvar.notify_one();
+               
+        drop(started);
+        thread::sleep(time::Duration::from_secs(1)); 
+    }    
+}
+
+
+
 fn main() {
-    println!("Hello, world!");
+    thread_wake_up_time();
+    
 }
