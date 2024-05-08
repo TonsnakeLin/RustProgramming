5934de1f9af88f73641c46475042dd4da3ae7ec3
TonsnakeLin <lpbgytong@163.com>
2024-05-08 10:04:37 -0400

    update
    Signed-off-by: TonsnakeLin <lpbgytong@163.com>

---
+++ tokio_example/components/my_redis/src/lib.rs
@@ -0,0 +1,2 @@
+mod redis_get_set;
+pub use self::redis_get_set::redis_get_set;
--- tokio_example/src/main.rs
+++ tokio_example/src/main.rs
@@ -1,51 +1,8 @@
-use tokio::net::TcpListener;
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
-
-use futures::executor::block_on;
-
-
-#[allow(dead_code)]
-async fn tcp_receive_send() -> Result<(), Box<dyn std::error::Error>> {
-    let listener = TcpListener::bind("127.0.0.1:8080").await?;
-
-    loop {
-        let (mut socket, _) = listener.accept().await?;
-        tokio::spawn(async move {
-            let mut buf = [0; 1024];
-
-            // In a loop, read data from the socket and write the data back.
-            loop {
-                let n = match socket.read(&mut buf).await {
-                    // socket closed
-                    Ok(n) if n == 0 => return,
-                    Ok(n) => n,
-                    Err(e) => {
-                        eprintln!("failed to read from socket; err = {:?}", e);
-                        return;
-                    }
-                };
-
-                // Write the data back
-                if let Err(e) = socket.write_all(&buf[0..n]).await {
-                    eprintln!("failed to write to socket; err = {:?}", e);
-                    return;
-                }
-            }
-        });
-    }    
-}
-
-async fn hello_world() {
-    println!("hello world");
-}
-
-fn print_hello_world() {
-    block_on(hello_world())
-}
+use mini_redis::Result;
+use my_redis::redis_get_set;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>>{
-
-    print_hello_world();
+async fn main() -> Result<()>{
+    redis_get_set().await?;
     Ok(())
 }
